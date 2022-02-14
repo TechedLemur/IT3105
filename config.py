4c527464272aadbdf2262999ffa25b07831432ab
Jørgen Rosager <joergen.rosager@gmail.com>
2022-02-14 12:01:51 +0100

    fix gravity

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     class PoleWorldConfig:
         POLE_LENGTH = 0.5  # Range [0.1, 1.0]
         POLE_MASS = 0.25  # Range [0.05, 0.5]
-        GRAVITY = 9.81  # Range [5, 15]
+        GRAVITY = -9.81  # Range [-5, -15]
         TIMESTEP = 0.05  # Range [0.01, 0.1]
         DISCRETIZATION = 8
 
